68cdb084b557324e40dfa9037e46e8274c1aed64
EmilSkov <EmilSkov@users.noreply.github.com>
2018-11-16 08:43:00 +0100

    Update d.16/11

--- Sweetbot/stylesheets/javastyle.js
+++
@@ -1,9 +0,0 @@
-// JavaScript Document
-function closedown() {
-    var x = document.getElementById("sidebanner");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
-    }
-}
---
+++ stylesheets/javastyle.js
@@ -0,0 +1,9 @@
+// JavaScript Document
+function closedown() {
+    var x = document.getElementById("sidebanner");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
